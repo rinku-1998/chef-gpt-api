6eb1f6d76a333f08fb001d70d479c3fbbad02b11
Rinku <shark4rkcoding@gmail.com>
2024-01-04 11:23:00 +0800

    feat: 新增攔截 Token 例外

--- app/exception_handler.py
+++ app/exception_handler.py
@@ -1,6 +1,7 @@
 from app.enum.status_msg import StatusMsg
 from app.exception.user_exception import UserExistException, UserNotExistException
 from app.exception.password_exception import PasswordNotStrongException, WrongPasswordException
+from app.exception.token_exception import TokenNotExistException, MissingTokenException
 from app.model.base_res import BaseRes
 from fastapi import FastAPI, Request
 from fastapi.exceptions import RequestValidationError
@@ -68,6 +69,26 @@ def attach_exception_handlers(app: FastAPI) -> FastAPI:
         res = BaseRes(msg=StatusMsg.WRONG_PASSWORD.value)
 
         return JSONResponse(jsonable_encoder(res), status_code=400)
+
+    # Token 不存在
+    @app.exception_handler(TokenNotExistException)
+    async def token_not_exist_exception_handler(
+            request: Request,
+            exception: TokenNotExistException) -> JSONResponse:
+
+        res = BaseRes(msg=StatusMsg.TOKEN_NOT_EXIST.value)
+
+        return JSONResponse(jsonable_encoder(res), status_code=401)
+
+    # 缺少 Token
+    @app.exception_handler(MissingTokenException)
+    async def missing_token_exception_handler(
+            request: Request,
+            exception: MissingTokenException) -> JSONResponse:
+
+        res = BaseRes(msg=StatusMsg.TOKEN_MISSING.value)
+
+        return JSONResponse(jsonable_encoder(res), status_code=401)
 
     # 全局例外
     @app.exception_handler(Exception)
