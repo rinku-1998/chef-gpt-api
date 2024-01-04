a557539ae367720f58d84f217b6822daa542684d
Rinku <shark4rkcoding@gmail.com>
2024-01-04 15:36:45 +0800

    refactor: 調整 CORS Middleware 載入順序

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from app.extension import SessionLocal
 from app.exception_handler import attach_exception_handlers
 from app.cors_middleware import add_cors_middleware
 
@@ -7,7 +8,6 @@ def create_app() -> FastAPI:
 
     # 1. 建立物件
     app = FastAPI()
-    app = add_cors_middleware(app)
 
     # 2. 路由
     from app.router.user_router import router as user_router
@@ -20,5 +20,6 @@ def create_app() -> FastAPI:
     app = attach_exception_handlers(app)
 
     # 4. Middleware
+    app = add_cors_middleware(app)
 
     return app
