a772296f4ede9b18cd3b1a6f39588d0f3dd62154
Rinku <shark4rkcoding@gmail.com>
2024-01-04 16:31:33 +0800

    feat: 新增日誌格式

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
-from fastapi import FastAPI
+import logging
+import sys
 from app.extension import SessionLocal
 from app.exception_handler import attach_exception_handlers
 from app.cors_middleware import add_cors_middleware
+from app.logging_handler import format_record, InterceptHandler
+from fastapi import FastAPI
+from loguru import logger
 
 
 def create_app() -> FastAPI:
@@ -9,17 +13,36 @@ def create_app() -> FastAPI:
     # 1. 建立物件
     app = FastAPI(title='Chef GPT API', version='0.1.0')
 
-    # 2. 路由
+    # 2. 日誌
+    # 格式
+    logger.configure(handlers=[{
+        'sink': sys.stdout,
+        'level': logging.DEBUG,
+        'format': format_record
+    }])
+
+    # Uvicorn
+    logging.getLogger().handlers = [InterceptHandler()]
+    logging.getLogger('uvicorn.access').handlers = [InterceptHandler()]
+
+    # 新增儲存位置
+    logger.add('logs/run.log',
+               rotation='500MB',
+               encoding='utf-8',
+               enqueue=True,
+               retention='15 days')
+
+    # 3. 路由
     from app.router.user_router import router as user_router
     from app.router.conversation_router import router as conversation_router
 
     app.include_router(user_router)
     app.include_router(conversation_router)
 
-    # 3. 例外攔截
+    # 4. 例外攔截
     app = attach_exception_handlers(app)
 
-    # 4. Middleware
+    # 5. Middleware
     app = add_cors_middleware(app)
 
     return app
