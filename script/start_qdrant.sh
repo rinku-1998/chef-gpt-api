86435443c9d10e14b3301fa850ca65b5a1c90490
Rinku <shark4rkcoding@gmail.com>
2024-01-31 16:49:45 +0800

    chore: 調整 qdrant 啟動指令掛載目錄

--- script/start_qdrant.sh
+++ script/start_qdrant.sh
@@ -4,5 +4,5 @@ docker run -d \
         --name qdrant \
         --log-opt max-size=50m  \
         -p 6333:6333 -p 6334:6334 \
-        -v ~/docker_vokume//qdrant/:/qdrant/storage \
+        -v ~/docker_volume//qdrant/:/qdrant/storage \
         qdrant/qdrant
