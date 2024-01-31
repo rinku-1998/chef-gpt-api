b1904fd33dd4e34822a9386a3cd5dc717f178508
Rinku <shark4rkcoding@gmail.com>
2024-01-31 15:48:56 +0800

    chore: 新增 qdrant 啟動指令

---
+++ script/start_qdrant.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker run -d \
+        --name qdrant \
+        --log-opt max-size=50m  \
+        -p 6333:6333 -p 6334:6334 \
+        -v ~/docker_vokume//qdrant/:/qdrant/storage \
+        qdrant/qdrant
